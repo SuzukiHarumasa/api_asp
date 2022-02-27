0c96f3f4447d4a721fdf990ee3b89548f25f63a3
SuzukiHarumasa <sh14124869@gmail.com>
2022-02-27 10:11:08 +0900

    change run.py

--- modules.py
+++ modules.py
@@ -24,6 +24,33 @@ from bs4 import BeautifulSoup
 from typing import Union, List, Optional
 from gspread_dataframe import set_with_dataframe
 
+#アフィリエイトサイト（ASP）ごとのID
+
+login_id_a8 = 'curumi0821' #@param {type:"string"}
+login_mail_valuecommerce = 'info@dot-a.co.jp' #@param {type:"string"}
+login_mail_fukurou = 'info@dot-a.co.jp'
+login_mail_amazon = 'afb@curumi.jp'
+login_mail_rakuten = 'afb@curumi.jp'
+login_mail_afb = 'afbcurumi'
+login_mail_actrade = 'info@dot-a.co.jp'
+
+password_a8 = 'Curumi@1031'
+password_valuecommerce = 'Dt11dt11'
+password_fukurou = 'dotA1337'
+password_amazon = 'Curumi@1031'
+password_rakuten = 'Curumi@1031'
+password_afb = 'curumi1031'
+password_actrade = 'Dt11dt11'
+
+credentials = dict()
+credentials['A8'] = {'login_id': login_id_a8, 'password': password_a8}
+credentials['バリューコマース'] = {'login_id': login_mail_valuecommerce, 'password': password_valuecommerce}
+credentials['フクロウ'] = {'login_id': login_mail_fukurou, 'password': password_fukurou}
+credentials['アマゾン'] = {'login_id': login_mail_amazon, 'password': password_amazon}
+credentials['楽天'] = {'login_id': login_mail_rakuten, 'password': password_rakuten}
+credentials['afb'] = {'login_id': login_mail_afb, 'password': password_afb}
+credentials['アクトレ'] = {'login_id': login_mail_actrade, 'password': password_actrade}
+
 chromedriver_puth = '/app/.chromedriver/bin/chromedriver'
 df_dict = {
 'アマゾン':'data_amazon',
@@ -814,6 +841,10 @@ class GSheets:
         except:
             print('データが取得できませんでした')
             
+
+key = '17luM78MU8aEOqKIOd8F7490pol1HToe0NK12K_9pEVc'
+gs = GSheets(key)
+
 def get_daliy_sales(asp_name):
     my_asp = asp_instance(asp_name, credentials[asp_name]['login_id'], credentials[asp_name]['password'])
     
